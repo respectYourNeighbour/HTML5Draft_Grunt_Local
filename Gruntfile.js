cf5ad58ce4a3379e7b3849e676f5d4de0aa281f5
respectYourNeighbour <philip_luca@yahoo.com>
2014-12-05 10:27:08 +0200

    changes in GruntFile, order for concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
         separator: ';'
       },
       js: {
-        src: 'js/**/*.js', 
+        src: ['js/libs/jquery-1.11.1.js', 'js/libs/bootstrap.js', 'js/script.js'],
         dest: 'dest/deletable/<%= pkg.name %>.js'
       },
       css: {
